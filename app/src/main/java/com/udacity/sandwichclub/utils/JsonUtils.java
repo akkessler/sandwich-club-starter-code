46a2613a22ad3cb4c2a01b703aa9c192d0663e88
Andy Kessler <akkessler@users.noreply.github.com>
2018-04-30 23:53:03 -0500

    Implement Sandwich JSON parser

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,32 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import java.util.Arrays;
+
 public class JsonUtils {
+    
+    private static final String MAIN_NAME_KEY = "mainName";
+    private static final String AKA_KEY = "alsoKnownAs";
+    private static final String ORIGIN_KEY = "placeOfOrigin";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String IMAGE_KEY = "image";
+    private static final String INGREDIENTS_KEY = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        int mainNameIndex = json.indexOf(MAIN_NAME_KEY) + MAIN_NAME_KEY.length() + 3; // move past ":"
+        int akaIndex = json.indexOf(AKA_KEY) + AKA_KEY.length() + 3; // move past ":[
+        int originIndex = json.indexOf(ORIGIN_KEY) + ORIGIN_KEY.length() + 3; // move past ":"
+        int descriptionIndex = json.indexOf(DESCRIPTION_KEY) + DESCRIPTION_KEY.length() + 3; // move past ":"
+        int imageIndex = json.indexOf(IMAGE_KEY) + IMAGE_KEY.length() + 3; // move past ":"
+        int ingredientsIndex = json.indexOf(INGREDIENTS_KEY) + INGREDIENTS_KEY.length() + 3; // move past ":[
+
+        String mainName = json.substring(mainNameIndex, json.indexOf('"', mainNameIndex));
+        String[] alsoKnownAs = json.substring(akaIndex, json.indexOf(']', akaIndex)).replace("\"", "").split(",");
+        String placeOfOrigin = json.substring(originIndex, json.indexOf('"', originIndex));
+        String description = json.substring(descriptionIndex, json.indexOf('"', descriptionIndex));
+        String image = json.substring(imageIndex, json.indexOf('"', imageIndex));
+        String[] ingredients = json.substring(ingredientsIndex, json.indexOf(']', ingredientsIndex)).replace("\"", "").split(",");
+
+        return new Sandwich(mainName, Arrays.asList(alsoKnownAs), placeOfOrigin, description, image, Arrays.asList(ingredients));
     }
 }

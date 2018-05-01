672500bab24db50e9cc83e8afcbd511d33d4c3cb
Andy Kessler <akkessler@users.noreply.github.com>
2018-05-01 00:27:38 -0500

    Populate TextViews with sandwich data

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +58,15 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich s) {
+        TextView tvAlsoKnown = findViewById(R.id.also_known_tv);
+        TextView tvIngredients = findViewById(R.id.ingredients_tv);
+        TextView tvOrigin = findViewById(R.id.origin_tv);
+        TextView tvDescription = findViewById(R.id.description_tv);
 
+        tvAlsoKnown.setText(TextUtils.join(", ", s.getAlsoKnownAs()));
+        tvIngredients.setText(TextUtils.join(", ", s.getIngredients()));
+        tvOrigin.setText(s.getPlaceOfOrigin());
+        tvDescription.setText(s.getDescription());
     }
 }
